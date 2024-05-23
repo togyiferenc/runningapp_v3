50acf4fa1e42443b6656c5f550cf21f4dc73aa5f
= <=>
2024-05-23 23:25:06 +0200

    V3

---
+++ src/main/java/com/example/runningapp/RunningappApplication.java
@@ -0,0 +1,12 @@
+package com.example.runningapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunningappApplication.class, args);
+    }
+}
